45cfee4db4c70ec005f1d5096d5745bfee71e719
carlajoha <carlajoha.work@gmail.com>
2023-03-14 17:28:28 -0400

    getById

--- Proyecto/api/src/controllers/getById.js
+++ Proyecto/api/src/controllers/getById.js
@@ -3,8 +3,19 @@ const { Product } = require("../db.js")
 const { data } = require("../data/data")
 
 
-const getById = (dataId, dbId) => {
-    if(dataId) {
-        
+const getById = async (dbId) => {
+    const productDetail = {};
+
+    if(dbId) {
+        productDetail = await Product.findByPK(dbId)
+    }
+    productDetail = {
+        name,
+        description,
+        selling_price,
+        images,
+        average_rating,
+        availability,
+        sku
     }
 }
