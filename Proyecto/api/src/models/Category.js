cad7c486f4e7130f6c547a584d6853cb45e508b8
Rocio <romaturano96@gmail.com>
2023-03-14 16:18:43 -0300

    Se modifica el name  por type de categoria que contiene shoes, ropa y accesorios

--- Proyecto/api/src/models/Category.js
+++ Proyecto/api/src/models/Category.js
@@ -4,9 +4,13 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('Category', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+    type: {
+      type: DataTypes.ENUM(
+        "Clothing",
+        "Shoes",
+        "Accessories"
+        
+      ),
     },
     description:{
       type: DataTypes.TEXT,
