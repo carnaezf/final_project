02ba0abf604c47b94e23e1dac1936b7cc02e0b45
Rocio <romaturano96@gmail.com>
2023-03-15 14:00:08 -0300

    Se realizan modelos y cambios

--- Proyecto/api/src/models/Coustomer.js
+++ Proyecto/api/src/models/Coustomer.js
@@ -4,6 +4,7 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('Costumer', {
+    //console.Log("hola")
     userId:{
         type: DataTypes.INTEGER,
         allowNull: false,
