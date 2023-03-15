552ba556640511a9efbbb5a02b6fa764f7f85f52
Giuliana Schiebelbein <giulianaschiebelbein2@gmail.com>
2023-03-15 15:12:06 -0300

    cambios navbar

--- Proyecto/client/src/Components/NavBar/NavBar.jsx
+++ Proyecto/client/src/Components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
-import React, {useState} from React;
-import { Link, NavLink } from 'react-router-dom';
+import React  from React;
+import { Link } from 'react-router-dom';
 
 export default function navBar(){
     return(
--- Proyecto/client/src/Views/Home/Home.jsx
+++ Proyecto/client/src/Views/Home/Home.jsx
@@ -1,23 +1,22 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import {Link} from "react-router-dom";
+import NavBar from "../../Components/NavBar/NavBar";
+
 
 // import { useEffect, useState } from "react";
 
 
 export default function Home(){
-   
-    
-
-    
-    return (
-        <>
-        <h1>Home</h1>
-        <Link to = "home/products">
+   return (
+        <div> 
+            <NavBar/>
+            <h1>Home</h1>
+            <Link to = "home/products">
             <button>
                 Productos
             </button>
-        </Link>
-        </>
+         </Link>
+        </div>
                
            
     )
