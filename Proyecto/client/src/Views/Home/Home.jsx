d24b7d1fa2c4a35d22b143e08b8df5e7148f83cd
Colmeex <colmeex@gmail.com>
2023-03-15 11:44:36 -0400

    renderizado home y cambios en App.js

--- Proyecto/client/src/Views/Home/Home.jsx
+++ Proyecto/client/src/Views/Home/Home.jsx
@@ -1,1 +1,24 @@
-/******* */
+import React from "react";
+import {Link} from "react-router-dom"
+
+// import { useEffect, useState } from "react";
+
+
+export default function Home(){
+   
+    
+
+    
+    return (
+        <>
+        <h1>Home</h1>
+        <Link to = "home/products">
+            <button>
+                Productos
+            </button>
+        </Link>
+        </>
+               
+           
+    )
+}
