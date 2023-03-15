46906ade14913d3a48d5f46c5fc4ef7eab0cd30e
Andres Aldao <andresaldao95@gmail.com>
2023-03-15 00:25:51 -0300

    backend funcionando

--- Proyecto/api/src/db.js
+++ Proyecto/api/src/db.js
@@ -1,40 +1,40 @@
-require('dotenv').config()
-const { Sequelize } = require('sequelize')
-const fs = require('fs')
-const path = require('path')
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env
-const product = require('./models/Product')
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const product = require("./models/Product");
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/products`,
   {
     logging: false,
-    native: false
+    native: false,
   }
-)
-const basename = path.basename(__filename)
-const modelDefiners = [product]
+);
+const basename = path.basename(__filename);
+const modelDefiners = [product];
 
-fs.readdirSync(path.join(__dirname, '/models'))
+fs.readdirSync(path.join(__dirname, "/models"))
   .filter(
-    file =>
-      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
   )
-  .forEach(file => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)))
-  }) 
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
+  });
 
-modelDefiners.forEach(model => model(sequelize))
+modelDefiners.forEach((model) => model(sequelize));
 
-let entries = Object.entries(sequelize.models)
-let capsEntries = entries.map(entry => [
+let entries = Object.entries(sequelize.models);
+let capsEntries = entries.map((entry) => [
   entry[0][0].toUpperCase() + entry[0].slice(1),
-  entry[1]
-])
-sequelize.models = Object.fromEntries(capsEntries)
+  entry[1],
+]);
+sequelize.models = Object.fromEntries(capsEntries);
 
-const { Product } = sequelize.models
+const { Product } = sequelize.models;
 
 module.exports = {
   ...sequelize.models,
-  conn: sequelize
-}
+  conn: sequelize,
+};
--- Proyecto/api/src/models/Product.js
+++ Proyecto/api/src/models/Product.js
@@ -1,46 +1,51 @@
-
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('Product', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description:{
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    selling_price:{
-      type: DataTypes.FLOAT,
-    allowNull: false,
-    },
-    images:{
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    average_rating:{
-      type: DataTypes.FLOAT,
-    allowNull: false,
-    },
-    availability:{
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    sku:{
-      type: DataTypes.STRING,
-      allowNull: false,
-      primaryKey:true,
-    },
-    
- reviews_count: {
-      type: DataTypes.INTEGER
-    },
+  sequelize.define(
+    "Product",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      sellingPrice: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+      images: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        // allowNull: false,
+      },
+      average_rating: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+      // availability: {
+      //   type: DataTypes.STRING,
+      //   allowNull: false,
+      // },
+      sku: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+      },
+      category: {
+        type: DataTypes.STRING,
+        // allowNull: false,
+      },
 
-  },{
-    timestamps: false
-});
+      // reviews_count: {
+      //   type: DataTypes.INTEGER,
+      // },
+    },
+    {
+      timestamps: false,
+    }
+  );
 };
-
