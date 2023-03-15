ebac563728a96253c21b6b2403efbb9a4f326157
Rocio <romaturano96@gmail.com>
2023-03-15 12:32:00 -0300

    agregado de modelo user- costumer- review y modificacion de propiedad status del modelo orden status: rechazada, preparacion, enviada

---
+++ Proyecto/api/src/models/User.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('User', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: {
+            msg: "El email tiene que ser un correo valido",
+          },
+        },
+      },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    admin: {
+        type:DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue:false,
+      },
+
+
+  },{
+    timestamps: false
+});
+};
