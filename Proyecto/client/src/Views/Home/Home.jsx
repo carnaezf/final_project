5393b69c184d80baa27ad87a92894adf4efdc674
Wilcodig <wrvfce@gmail.com>
2023-03-15 13:56:57 -0300

    Reducer-Actions-Home

--- Proyecto/client/src/Views/Home/Home.jsx
+++ Proyecto/client/src/Views/Home/Home.jsx
@@ -1,1 +1,46 @@
-/******* */
+
+import Paginated from "../../Components/Paginated";
+import { useEffect, useState } from "react";
+import { useDispatch,useSelector} from "react-redux"; //mis hooks
+
+const Home= ()=>{
+
+  const dispatch= useDispatch();
+  useEffect(()=>{
+    dispatch(getProducts())
+  },[dispatch]);
+
+
+  const allProducts= useSelector(state=> state.products) ;
+  const[pageActual, setPageActual]= useState (1); //numero de pagina donde estoy
+  const[productsViews, setProductsViews]= useState (10);
+  const lastIndex= pageActual * productsViews; //segundo parametro
+  const firstIndex= lastIndex - productsViews; //primer parameto
+  const maximumPage= Math.ceil(allProducts.length/productsViews); //nose si sera par o inpar
+  const newStateProducts= allProducts.slice(firstIndex,lastIndex);
+
+
+  const pagin=(pageNumber)=>{ //rendirizamos
+    setPageActual(pageNumber); //pasamos los numero de las paginas para modificar el estado local
+  }
+
+  return (
+   <div className={styles.home}>
+      <NavBar pagin={pagin}/>//!Cambiar componente
+      
+      <Filter pagin={pagin}/>
+    
+      <Paginated maximumPage={maximumPage} pagin={pagin} />
+    
+      <Cards newStateProducts={newStateProducts}/>
+
+      <p>Pagina {pageActual}</p>
+   </div>
+  )
+};
+
+export default Home;
+
+
+
+
