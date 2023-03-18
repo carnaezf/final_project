4c14822b2a8e79b813941f720c6bdfac1f11853a
Cesar Arnaez Flores <c_arnaez.flores@hotmail.com>
2023-03-18 10:25:42 -0300

    Implementacion de estructura formik rendered prop

--- Proyecto/client/src/Views/AdminForm/CreateProducts.jsx
+++ Proyecto/client/src/Views/AdminForm/CreateProducts.jsx
@@ -1,40 +1,45 @@
-import react from 'react';
+import React from 'react';
+import { Formik } from 'formik';
 
 const CreateProduct = () => {
     return (
         <>
-            <form className='ceateProductName'>
-                <div>
-                    <label htmlFor="id">Product id: </label>
-                    <input type="text" name="id" id="id" placeholder='Enter id product'/>
-                </div>
+            <Formik>
+                {() => (
+                    <form className='ceateProductName'>
+                    <div>
+                        <label htmlFor="id">Product id: </label>
+                        <input type="text" name="id" id="id" placeholder='Enter id product'/>
+                    </div>
 
-                <div>
-                    <label htmlFor="name">Product Name: </label>
-                    <input type="text" name="name" id="name"  placeholder='Enter product name'/>
-                </div>
+                    <div>
+                        <label htmlFor="name">Product Name: </label>
+                        <input type="text" name="name" id="name"  placeholder='Enter product name'/>
+                    </div>
 
-                <div>
-                    <label htmlFor="sellingPrice">Selling Price: </label>
-                    <input type="number" name="sellingPrice" id="sellingPrice" placeholder='Enter selling price'/>
-                </div>
+                    <div>
+                        <label htmlFor="sellingPrice">Selling Price: </label>
+                        <input type="number" name="sellingPrice" id="sellingPrice" placeholder='Enter selling price'/>
+                    </div>
 
-                <div>
-                    <label htmlFor="description">Product Description: </label>
-                    <input type="text" name="description" id="description"  placeholder='Enter product description'/>
-                </div>
+                    <div>
+                        <label htmlFor="description">Product Description: </label>
+                        <input type="text" name="description" id="description"  placeholder='Enter product description'/>
+                    </div>
 
-                <div>
-                    <label htmlFor="category">Product Category: </label>
-                    <input type="text" name="category" id="category" placeholder='Enter product category'/>
-                </div>
+                    <div>
+                        <label htmlFor="category">Product Category: </label>
+                        <input type="text" name="category" id="category" placeholder='Enter product category'/>
+                    </div>
 
-                <div>
-                    <label htmlFor="images">Images: </label>
-                    <input type="file" name="images" id="images"/>
-                </div>
-                <button type='submit'>Send</button>
-            </form>
+                    <div>
+                        <label htmlFor="images">Images: </label>
+                        <input type="file" name="images" id="images"/>
+                    </div>
+                    <button type='submit'>Send</button>
+                </form>
+                )}
+            </Formik>
         </>
     );
 }
