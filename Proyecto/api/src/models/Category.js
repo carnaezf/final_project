01a36b8fd13e138fe5d28ed595bb8bbd4733836c
Nahir-Arrieta <nahir.arrieta@hotmail.com>
2023-03-20 23:17:58 -0300

    update Products

--- Proyecto/api/src/models/Category.js
+++ Proyecto/api/src/models/Category.js
@@ -1,24 +1,27 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('Category', {
-    type: {
-      type: DataTypes.ENUM(
-        "clothing",
-        "shoes",
-        "accessories"
-        
-      ),
+  sequelize.define(
+    "Category",
+    {
+      type: {
+        type: DataTypes.ENUM(
+          "clothing",
+          "shoes",
+          "accessories",
+          "sports",
+          "sustainable collection"
+        ),
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
     },
-    description:{
-      type: DataTypes.TEXT,
-      allowNull: false,
+    {
+      timestamps: false,
     }
-
-
-  },{
-    timestamps: false
-});
+  );
 };
