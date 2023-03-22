c2ea85f20421f44cfae626e167746cdffaa787b1
Colmeex <colmeex@gmail.com>
2023-03-22 16:18:34 -0400

    Cambios home y nav

--- Proyecto/client/src/Components/NavBar/NavBar.jsx
+++ Proyecto/client/src/Components/NavBar/NavBar.jsx
@@ -59,7 +59,7 @@ export default function NavBar(props) {
         </ul>
       </div>
   </div>
-{/*         
+        
         <div className="dropdown dropdown-end">
       <label tabIndex={0} className="btn btn-ghost btn-circle avatar ml-4 mr-4">
         <div className="w-10 rounded-full border-trasparent hover:border">
@@ -72,7 +72,9 @@ export default function NavBar(props) {
       <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
         <li className="justify-between"> <Link to='/login-form'> <a>Login <span className="badge">New</span> </a></Link></li>
         <li className="justify-between"> <Link to='/createProduct'> <a>Shopping Cart</a></Link></li>
-      </ul> */}
+      </ul>
+        </div>
+            </div>
     </div>
     
 
