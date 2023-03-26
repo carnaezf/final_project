90938c67854b8bbe938408fadb2592379770e38f
carlajoha <carlajoha.work@gmail.com>
2023-03-26 15:13:09 -0400

    envio de email por registro

--- Proyecto/api/src/controllers/mailsControllers/mail-register.js
+++ Proyecto/api/src/controllers/mailsControllers/mail-register.js
@@ -5,6 +5,10 @@ const mailRegister = async (req, res) => {
 
     const { email, name } = req.body;
 
+    const html = `<div>
+        <img src="" alt="wellcome"/>   
+    </div>`;
+
     // console.log(req.body);
   const mailRegister = nodeoutlook.sendEmail({
         host: "smtp.office365.com",
@@ -18,7 +22,7 @@ const mailRegister = async (req, res) => {
         to: email, //ARRAY user.email
         subject:`¡Welcome ${name} !`,// "¡Bienvenido!"
         text: "Thanks for choosing us!",//`${req.body.name} gracias por registarse en Sports, ya puede disfrutar de la experiencia completa`,
-        html: "<b>Thanks for choosing us!</b>",
+        html: "<div><b>Thanks for choosing us!</b><div>",
       
         onError: (e) => console.log(e),
         onSuccess: (i) => console.log("Mail sent", i)
