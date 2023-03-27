d4f2dd14bfe06cda1df031c7adeaa7ba3c0e1614
Cesar Arnaez Flores <carnaez@enjoy.cl>
2023-03-27 09:58:13 -0300

    Tailwindcss basicos en shoppingBag

--- Proyecto/client/src/Components/ShoppingBag/ShoppingBag.jsx
+++ Proyecto/client/src/Components/ShoppingBag/ShoppingBag.jsx
@@ -18,24 +18,24 @@ const ShoppingBag = () => {
     );
 
     return (
-        <div>
-
-            <div>
-                <NavBar />
+        <div className="bg-gray-100 min-h-screen">
+            <NavBar />
+            <div className="container mx-auto py-8">
+                <div className="bg-white p-4 shadow-md rounded-md flex-col">
+                    <div className="text-lg font-semibold mb-4">Shopping Bag</div>
+                    <div className="flex justify-between items-center border-b pb-4">
+                        <div className="font-semibold">Items in cart:</div>
+                        <div>{quantity}</div>
+                    </div>
+                    <div className="flex justify-between items-center border-b py-4 mt-4">
+                        <div className="font-semibold">Total:</div>
+                        <div>${totalPrice}</div>
+                    </div>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4" onClick={() => console.log(shoppingBag)}>Checkout</button>
+                </div>
             </div>
-            <div>
-            <div>Items in cart: {quantity}</div>
-            <div>Total: ${totalPrice}</div>
-                <button onClick={() => console.log(shoppingBag)}>Checkout</button>
-          </div>
-          </div>
-    )
-
-}
-
-
+        </div>
+    );
+};
 
 export default ShoppingBag;
-
-
-
