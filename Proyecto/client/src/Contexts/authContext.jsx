dfdcdbb99e84ba05eac50285dfadf0fdf9894a09
Colmeex <colmeex@gmail.com>
2023-03-29 11:27:42 -0400

    Login y registro usuarios

---
+++ Proyecto/client/src/Contexts/authContext.jsx
@@ -0,0 +1,43 @@
+
+import { createContext,useContext, useEffect,useState } from "react";
+import { createUserWithEmailAndPassword,signInWithEmailAndPassword, onAuthStateChanged, signOut, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../firebase"
+
+export const authContext = createContext()
+
+export const useAuth = () => {
+        const context = useContext(authContext)
+        return context
+}
+
+export function AuthProvider({ children }){
+
+    const [user, setUser] = useState(null)
+
+    const signUp = (email, password) => 
+    createUserWithEmailAndPassword(auth,email,password)
+
+    const login = (email,password) => 
+    signInWithEmailAndPassword (auth, email, password)
+    
+    const logout = () => signOut(auth)
+
+    const loginWithGoogle = () => {
+        const googleProvider = new GoogleAuthProvider()
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    useEffect(() => {
+        console.log("ya cargo")
+        onAuthStateChanged(auth, currentUser => {
+            console.log(currentUser)
+            setUser(currentUser)
+        })
+    },[])
+
+    return (
+        <authContext.Provider value={{ signUp, login, user, logout, loginWithGoogle}}>
+            {children}
+        </authContext.Provider>
+    )
+}
