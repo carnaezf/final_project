2fe9d916346ea2bf0b9dca2c70c9e6c8bdb03775
Nahir-Arrieta <nahir.arrieta@hotmail.com>
2023-03-31 14:29:15 -0300

    cambios que funcionan

--- Proyecto/client/src/Redux/store.js
+++ Proyecto/client/src/Redux/store.js
@@ -1,13 +1,37 @@
+// import { createStore, applyMiddleware, compose } from "redux";
+// // import { composeWithDevTools } from "redux-devtools-extension";
+// import thunkMiddleware from "redux-thunk";
+// import rootReducer from "./reducer";
+
+// const composeEnhancer = window.REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose();
+
+// const store = createStore(
+//   rootReducer,
+//   composeEnhancer(applyMiddleware(thunkMiddleware))
+// );
+
+// export default store;
+
 import { createStore, applyMiddleware, compose } from "redux";
-// import { composeWithDevTools } from "redux-devtools-extension";
 import thunkMiddleware from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import rootReducer from "./reducer";
 
-const composeEnhancer = window.REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose();
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  whitelist: ["user"], // solo se persiste el estado de shoppingCart
+};
 
-const store = createStore(
-  rootReducer,
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const composeEnhancer = window.REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
+
+export const store = createStore(
+  persistedReducer,
   composeEnhancer(applyMiddleware(thunkMiddleware))
 );
 
-export default store;
+export const persistor = persistStore(store);
