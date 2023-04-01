83f243a0de6ead833a03089b5bfdde30566d77b7
Cesar Arnaez Flores <carnaez@enjoy.cl>
2023-04-01 19:14:52 -0300

    cambios ShoppingBagContext

--- Proyecto/client/src/Contexts/ShoppingBagContext.jsx
+++ Proyecto/client/src/Contexts/ShoppingBagContext.jsx
@@ -2,14 +2,15 @@
 // No comments
 import React, { createContext, useState } from "react";
 
-
-
-
 export const ShoppingBagContext = createContext(null);
 
 export const ShoppingBagProvider = ({ children }) => {
   const [shoppingBag, setShoppingBag] = useState([]);
   //console.log('Estado de shoppingBag en el contexto:', shoppingBag);
+
+  const addProduct = (product, size) => {
+    setShoppingBag([...shoppingBag, { product, size }]);
+  };
 
   return (
     <ShoppingBagContext.Provider value={[shoppingBag, setShoppingBag]}>
