267fcd4c39317db756d326ca059b0096ae874bd0
Nahir-Arrieta <nahir.arrieta@hotmail.com>
2023-04-03 23:25:37 -0300

    login de google

--- Proyecto/api/index.js
+++ Proyecto/api/index.js
@@ -25,4 +25,5 @@ const { conn } = require("./src/db.js");
 conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
+  });
 });
