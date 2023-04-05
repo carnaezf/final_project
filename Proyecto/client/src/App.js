14897055d0e2e245238fbb96aa4f0ecb093cb0aa
Cesar Arnaez Flores <88009193+carnaezf@users.noreply.github.com>
2023-04-04 22:12:57 -0400

    Update App.js

--- Proyecto/client/src/App.js
+++ Proyecto/client/src/App.js
@@ -29,7 +29,7 @@ import ProfileUser from "./Components/ProfileUser/ProfileUser";
 // // Deploy
 import axios from "axios";
 // axios.defaults.baseURL = 'http://localhost:3001/'
-axios.defaults.baseURL='http://finalproject-production-58fc.up.railway.app'
+axios.defaults.baseURL='https://finalproject-production-58fc.up.railway.app'
 
 function App() {
   //const location =useLocation();
