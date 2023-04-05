1baf573c7e49074a6966823e7bcddad01d9148bd
Cesar Arnaez Flores <88009193+carnaezf@users.noreply.github.com>
2023-04-04 22:25:14 -0400

    Update index.js

--- Proyecto/api/index.js
+++ Proyecto/api/index.js
@@ -24,8 +24,8 @@ const { PORT } = process.env
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log("%s listening at ", PORT); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log("%s listening at ", process.env.PORT); // eslint-disable-line no-console
   });
 
 });
